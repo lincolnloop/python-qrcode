36199d9d8976e1ecfd9314e65ac4288199756dad
Chris Beaven <smileychris@gmail.com>
2011-11-10 20:58:47 +1300

    Add example

--- qrcode/__init__.py
+++ qrcode/__init__.py
@@ -1,2 +1,15 @@
 from qrcode.main import QRCode
 from qrcode.constants import *
+
+
+def run_example():
+    qr = QRCode(20, ERROR_CORRECT_L)
+    qr.addData("http://www.lincolnloop.com")
+    qr.make()
+
+    im = qr.makeImage()
+    im.show()
+
+
+if __name__ == '__main__':
+    run_example()
