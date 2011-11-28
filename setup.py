864195cbd7c7520b50affbd942fda3e6b07ccf56
Chris Beaven <smileychris@gmail.com>
2011-11-28 14:52:10 +1300

    Bump version number to 2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='qrcode',
-    version='1.1',
+    version='2.0',
     url='https://github.com/lincolnloop/python-qrcode',
     #download_url='',
     description='QR Code image generator',
