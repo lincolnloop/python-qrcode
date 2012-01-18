d8511396d9d2ac9ad368afb3c9885e2814352e22
Chris Beaven <smileychris@gmail.com>
2012-01-18 13:05:00 +1300

    2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='qrcode',
-    version='2.1',
+    version='2.2',
     url='https://github.com/lincolnloop/python-qrcode',
     #download_url='',
     description='QR Code image generator',
