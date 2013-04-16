50badd38a0dccd7c08f80d43ea37715b65e084d2
Hugh Rawlinson <hugh@codeoclock.net>
2013-04-17 00:54:52 +0100

    addressed issues

--- qrcode/main.py
+++ qrcode/main.py
@@ -302,27 +302,26 @@ class QRCode:
                     inc = -inc
                     break
 
-    def getMatrix(self,mode=True):
+    def get_matrix(self):
         """
         Return the QR Code as a multidimensonal array
         If mode is set return data with border, else return
         data as is.
         """
-        code = self.modules
-        width = len(code)
-
-        if(mode):
-            for x in range(0,width+self.border*2):
-                if(x<self.border):
-                    code.insert(0,[False]*(width+(self.border*2)))
-                elif(x >= width+self.border):
-                    code.append([False]*(width+(self.border*2)))
-                else:
-                    for n in range(0,self.border):
-                        code[x].insert(0,False)
-                        code[x].append(False)
-
         if self.data_cache is None:
             self.make()
+
+        code = self.modules
+        
+        width = len(code)
+        for x in range(0,width+self.border*2):
+            if(x<self.border):
+                code.insert(0,[False]*(width+(self.border*2)))
+            elif(x >= width+self.border):
+                code.append([False]*(width+(self.border*2)))
+            else:
+                for n in range(0,self.border):
+                    code[x].insert(0,False)
+                    code[x].append(False)
 
         return code
