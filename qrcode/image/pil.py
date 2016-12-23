5bf532e53a3947508e037a9ab495e01b43226802
cryptogun <safetychinese@gmail.com>
2016-12-23 23:03:10 +0800

    Set calculated value for black fill_color. Saving another 1.4s.
    Each time you draw a black rectangle, PIL calls getcolor() to lookup 'black' and then return 0. Lookup 36931 times takes 1.428s on my computer. (41*41/2*51=42865 black rectangles on 51 qrcodes of version 6.)

--- qrcode/image/pil.py
+++ qrcode/image/pil.py
@@ -29,6 +29,9 @@ class PilImage(qrcode.image.base.BaseImage):
                 mode = "RGB"
         else:
             mode = "1"
+            # L mode (1 mode) color = (r*299 + g*587 + b*114)//1000
+            if fill_color.lower() == "black": fill_color = 0
+            if back_color.lower() == "white": back_color = 255
 
         img = Image.new(mode, (self.pixel_size, self.pixel_size), back_color)
         self.fill_color = fill_color
