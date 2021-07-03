6d55af7857cc9b72235b178c80db9c1863c6aafc
KmolYuan <pyslvs@gmail.com>
2021-07-03 08:10:25 +0800

    Adjust methods.

--- qrcode/image/numpy.py
+++ qrcode/image/numpy.py
@@ -1,7 +1,7 @@
 # Needed on case-insensitive filesystems
 from __future__ import absolute_import
 
-from numpy import full, uint8
+from numpy import ones, uint8
 
 import qrcode.image.base
 
@@ -10,30 +10,24 @@ class NpImage(qrcode.image.base.BaseImage):
 
     """NumPy image for QR code."""
 
-    def __init__(self, border, width, box_size, *args, **kwargs):
-        super(NpImage, self).__init__(border, width, box_size, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super(NpImage, self).__init__(*args, **kwargs)
 
     def new_image(self, **kwargs):
         """Build the image class."""
-        return full((self.pixel_size, self.pixel_size, 3), 255, dtype=uint8)
+        return ones((self.pixel_size, self.pixel_size, 3), dtype=uint8) * 255
 
     def drawrect(self, row, col):
         """Draw a single rectangle of the QR code."""
         (x, y), (x2, _) = self.pixel_box(row, col)
         for r in range(self.box_size):
-            self._img[y + r, x:x2 + 1] = (0, 0, 0)
-
-    def get_qimage(self, qimage):
-        """To QImage."""
-        height, width, color = self._img.shape
-        return qimage(
-            self._img.data,
-            width,
-            height,
-            color * height,
-            qimage.Format_RGB888
-        )
+            self._img[y + r, x:x2 + 1, :] = 0
 
     def save(self, stream, kind=None):
-        """Do nothing."""
-        pass
+        raise NotImplementedError
+
+    def __getattr__(self, name):
+        return getattr(self._img, name)
+
+    def __getitem__(self, ind):
+        return self._img[ind]
