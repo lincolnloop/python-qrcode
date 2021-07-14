faf95047951995e22918eb2f68719966f03d8f7d
KmolYuan <pyslvs@gmail.com>
2021-07-14 10:04:02 +0800

    Add PNG save function.

--- qrcode/image/numpy.py
+++ qrcode/image/numpy.py
@@ -10,6 +10,9 @@ class NpImage(qrcode.image.base.BaseImage):
 
     """NumPy image for QR code."""
 
+    kind = "PNG"
+    allowed_kinds = ("PNG",)
+
     def __init__(self, *args, **kwargs):
         super(NpImage, self).__init__(*args, **kwargs)
 
@@ -24,7 +27,32 @@ class NpImage(qrcode.image.base.BaseImage):
             self._img[y + r, x:x2 + 1, :] = 0
 
     def save(self, stream, kind=None):
-        raise NotImplementedError
+        """Save to PNG."""
+        from zlib import crc32, compress
+        from struct import pack
+
+        self.check_kind(kind)
+        h, w, _ = self._img.shape
+        img = ones((h, w, 4), dtype=uint8) * 255
+        img[:, :, :-1] = self._img
+        buf = img.tobytes()
+        w_byte = w * 4
+        raw_data = b''.join(b'\x00' + buf[span:span + w_byte]
+                            for span in range((h - 1) * w_byte, -1, -w_byte))
+
+        def png_pk(png_tag, data):
+            chunk_head = png_tag + data
+            return (pack("!I", len(data))
+                    + chunk_head
+                    + pack("!I", 0xFFFFFFFF & crc32(chunk_head)))
+
+        b = b''.join([
+            b'\x89PNG\r\n\x1a\n',
+            png_pk(b'IHDR', pack("!2I5B", w, h, 8, 6, 0, 0, 0)),
+            png_pk(b'IDAT', compress(raw_data, 9)),
+            png_pk(b'IEND', b'')])
+        with open(stream, 'wb') as f:
+            f.write(b)
 
     def __getattr__(self, name):
         return getattr(self._img, name)
