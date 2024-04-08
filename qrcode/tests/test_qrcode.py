708888083668b7c6f3ad4191ad485d8bd1e61263
Lukas Schulze <820580+lsgd@users.noreply.github.com>
2024-04-08 22:49:24 +0200

    Allow setting embedded image size in relation to the QR code width

--- qrcode/tests/test_qrcode.py
+++ qrcode/tests/test_qrcode.py
@@ -212,6 +212,14 @@ class QRCodeTests(unittest.TestCase):
         img.save(io.BytesIO())
 
     @unittest.skipIf(not pil_Image, "Requires PIL")
+    def test_render_styled_with_embeded_image_and_ratio(self):
+        embeded_img = pil_Image.new("RGB", (10, 10), color="red")
+        qr = qrcode.QRCode(error_correction=qrcode.ERROR_CORRECT_L)
+        qr.add_data(UNICODE_TEXT)
+        img = qr.make_image(image_factory=StyledPilImage, embeded_image=embeded_img, embeded_image_ratio=0.3)
+        img.save(io.BytesIO())
+
+    @unittest.skipIf(not pil_Image, "Requires PIL")
     def test_render_styled_with_embeded_image_path(self):
         tmpfile = os.path.join(self.tmpdir, "test.png")
         embeded_img = pil_Image.new("RGB", (10, 10), color="red")
