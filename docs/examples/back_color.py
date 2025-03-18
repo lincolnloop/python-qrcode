5b33d3c85aa9a56e5e2d317234eb3a4142105bed
KamilMatejuk <kamilmatejuk@gmail.com>
2025-03-18 16:40:47 +0100

    fix

--- docs/examples/back_color.py
+++ docs/examples/back_color.py
@@ -12,12 +12,12 @@ assert len(sys.argv) == 3, 'Usage: python back_color.py [data] [back_color]'
 DATA = sys.argv[1]
 
 # string or tuple[int, int, int]
-back_color = sys.argv[2]
-if '(' in back_color and ')' in back_color:
-    back_color = eval(back_color)
+BACK_COL = sys.argv[2]
+if '(' in BACK_COL and ')' in BACK_COL:
+    BACK_COL = eval(BACK_COL)
 
 qr = qrcode.QRCode()
 qr.add_data(DATA)
 qr.make(fit=True)
-img = qr.make_image(back_color=back_color)
+img = qr.make_image(back_color=BACK_COL)
 img.save("example.png")
--- docs/examples/fill_color.py
+++ docs/examples/fill_color.py
@@ -12,12 +12,12 @@ assert len(sys.argv) == 3, 'Usage: python fill_color.py [data] [fill_color]'
 DATA = sys.argv[1]
 
 # string or tuple[int, int, int]
-fill_color = sys.argv[2]
-if '(' in fill_color and ')' in fill_color:
-    fill_color = eval(fill_color)
+FILL_COL = sys.argv[2]
+if '(' in FILL_COL and ')' in FILL_COL:
+    FILL_COL = eval(FILL_COL)
 
 qr = qrcode.QRCode()
 qr.add_data(DATA)
 qr.make(fit=True)
-img = qr.make_image(fill_color=fill_color)
+img = qr.make_image(fill_color=FILL_COL)
 img.save("example.png")
