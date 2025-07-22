5071337d9fafadc7724694ec72a90a74fe4e5b6f
Martin Mahner <martin@mahner.org>
2025-07-22 12:23:20 +0200

    Ignore unused import

--- qrcode/tests/test_module.py
+++ qrcode/tests/test_module.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import pytest
 
 try:
-    from PIL import Image, ImageDraw
+    from PIL import Image, ImageDraw  # noqa: F401
 
     PIL_NOT_INSTALLED = False
 except ImportError:
